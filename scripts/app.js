23fe146972d50e552e4efe5d0463a4b9cdf2e1bd
DevTechJr <devtechjr@gmail.com>
2021-12-14 21:22:42 -0500

    almost done updating the ui

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,6 @@
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
 
 const cityData = async (city) => {
   const cityDetail = await getCity(city);
@@ -8,6 +10,19 @@ const cityData = async (city) => {
     cityDetail: cityDetail,
     weather: weather,
   };
+};
+
+const updateUI = (data) => {
+  const cityDetail = data.cityDetail;
+  const weather = data.weather;
+  // Update UI with weather
+  details.innerHTML = `
+    <h5 class="my-3 text-secondary">${data}</h5>
+    <div class="my-3 weatherDetails">${data}</div>
+    <div class="display-4 my-4">
+      <span>${data}</span>
+      <span>${data}&deg;C</span>
+    </div>`;
 };
 
 cityForm.addEventListener("submit", (e) => {
